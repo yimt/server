86f33686c62d228bc418b380881fa80609a8e090
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-02-27 11:03:17 +0100

    fixup! Add CSRF token controller to retrieve the current CSRF token
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- core/js/oc-requesttoken.js
+++ core/js/oc-requesttoken.js
@@ -1,6 +1,39 @@
-$(document).on('ajaxSend',function(elm, xhr, settings) {
-	if(settings.crossDomain === false) {
-		xhr.setRequestHeader('requesttoken', oc_requesttoken);
-		xhr.setRequestHeader('OCS-APIREQUEST', 'true');
+/* global OC, _ */
+
+(function($, OC, document) {
+
+	function addCSRFToken(elm, xhr, settings) {
+		if (settings.crossDomain === false) {
+			xhr.setRequestHeader('requesttoken', oc_requesttoken);
+			xhr.setRequestHeader('OCS-APIREQUEST', 'true');
+		}
 	}
-});
+
+	function handleExpiredCSRFToken(elm, xhr, settings) {
+		if (xhr.status !== 412 || settings.retried) {
+			// Ignore this error
+			return;
+		}
+
+		settings.retried = true;
+		var url = OC.generateUrl('/csrftoken');
+		$.ajax(url, {
+			success: function(resp) {
+				oc_requesttoken = resp.token;
+				OC.requestToken = resp.token;
+
+				// Retry request with same settings
+				$.ajax(settings);
+			},
+			// Call original error handler if possible
+			error: function() {
+				console.error('ERROR');
+				settings.error();
+			}
+		});
+	}
+
+	$(document).ajaxSend(addCSRFToken);
+	$(document).ajaxError(handleExpiredCSRFToken);
+
+})($, OC, document);
